6b22e1f7b99c9e65eb4366f081c4a948e17b95e9
Mems <Ongarbaevn@mail.ru>
2024-11-07 21:40:18 +0500

    video ligher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,17 +19,17 @@ export default function HomePage() {
 
   const videos = [
     {
-      src: "/3.mp4",
+      src: "/3-n.mp4",
       poster: "/poster3.jpg",
       preload: "metadata" as const
     },
     {
-      src: "/1.mp4",
+      src: "/1-n.mp4",
       poster: "/poster1.jpg",
       preload: "none" as const
     },
     {
-      src: "/2.mp4",
+      src: "/2-n.mp4",
       poster: "/poster2.jpg",
       preload: "none" as const
     }
