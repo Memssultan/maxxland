8d970b691ec12eeff320dfeeb391cd3a798f4e50
Memssultan <Ongarbaevn@mail.ru>
2024-11-12 02:39:46 +0500

    12

--- src/components/brands-section.tsx
+++ src/components/brands-section.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Image from "next/legacy/image";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ChevronDown, ChevronUp } from "lucide-react";
 
