c3b7d05d4f96cabe9b088e876da6c24e3f6acdf5
Memssultan <Ongarbaevn@mail.ru>
2024-11-16 21:52:11 +0500

    dark red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -233,7 +233,7 @@ export default function HomePage() {
               </div>
               <button 
                     onClick={() => setIsFormOpen(true)}
-                    className="mt-4 -translate-y-[75px] inline-flex items-center justify-center px-6 py-2 border border-white text-white text-sm hover:bg-white hover:text-black transition-colors duration-200"
+                    className="mt-4 -translate-y-[75px] inline-flex items-center justify-center px-6 py-2 border border-red-700 bg-red-700 text-white text-sm hover:bg-red-800 hover:border-red-800 transition-colors duration-200"
                   >
                     Подробнее
                   </button>
@@ -242,7 +242,7 @@ export default function HomePage() {
         </section>
 
         <Dialog open={isFormOpen} onOpenChange={setIsFormOpen}>
-  <DialogContent className="sm:max-w-[800px] max-h-[90vh] overflow-y-auto"> {/* Увеличен размер и добавлен скролл */}
+  <DialogContent className="sm:max-w-[800px] max-h-[90vh] overflow-y-auto">
     <BitrixForm />
   </DialogContent>
 </Dialog>
